95b7dee7c22f509188905b4d61232affabb4892b
aliatwood <aliatwood0104@gmail.com>
2026-02-17 15:54:45 +0100

    Uppdaterade vite.config
    La till animeringsidan i input

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        sass: resolve(__dirname, "src/sass.html")
+        sass: resolve(__dirname, "src/sass.html"),
+        animering: resolve(__dirname, "src/animering.html")
       }
     }
   }
